0bd5f4c3e7eb69304cbb0ad4114f8a977105c437
KinWaiCheuk <u3500684@connect.hku.hk>
2026-01-05 16:44:54 +0900

    added filename padding powershell script

---
+++ filename_padding.ps1
@@ -0,0 +1,18 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$TargetFolder
+)
+
+if (-not (Test-Path $TargetFolder)) {
+    Write-Error "Folder not found: $TargetFolder"
+    exit 1
+}
+
+Get-ChildItem -Path $TargetFolder -File | ForEach-Object {
+    if ($_.Name -match 'Voice_(\d+)(\..+)?') {
+        $newName = 'Voice_{0:D5}{1}' -f [int]$matches[1], $matches[2]
+        if ($_.Name -ne $newName) {
+            Rename-Item $_.FullName $newName
+        }
+    }
+}

31f923e82599924c593bb0dab480c0c1e1e701dd
KinWaiCheuk <u3500684@connect.hku.hk>
2026-01-05 18:24:13 +0900

    added add_extension.ps1

---
+++ add_extension.ps1
@@ -0,0 +1,10 @@
+﻿param (
+    [Parameter(Mandatory = $true)]
+    [string]$TargetFolder
+)
+
+Get-ChildItem -Path $TargetFolder -File |
+Where-Object { $_.Extension -eq "" } |
+ForEach-Object {
+    Rename-Item $_.FullName ($_.Name + ".mp3")
+}
